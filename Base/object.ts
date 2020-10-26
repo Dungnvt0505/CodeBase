8087adc1f5446b3c299fe535ea893111653ee8fc
capichi <thanhdungnv0505@gmail.com>
2020-10-26 18:19:20 +0700

    object

---
+++ Base/object.ts
@@ -0,0 +1,143 @@
+import _ from 'lodash'
+
+
+const people ={
+    name:'Hung Nguyen',
+    age:24,
+    job:'developer',
+    favourite:'Movies',
+    hair:'black'
+  }
+  
+const library =[
+    {
+      name:'Book 01',
+      author:'Zig 01',
+      is_read:false
+    },
+      {
+      name:'Book 02',
+      author:'Zig 02',
+      is_read:true
+    },
+      {
+      name:'Book 03',
+      author:'Zig 03',
+      is_read:false
+    },
+]
+
+/** function xoá prop của object
+ * @param obj: object, @param prop : prop cần xoá
+ * nếu có prop đó thì xoá 
+ */
+function deleteProp(object: Object, prop: string) : Object {
+    if (object[prop]){
+      delete object[prop]
+    }
+      return object
+   }
+ console.log(deleteProp(people,'favourite'))
+
+/** func hiển thị length 
+ * @param object : object đầu vào
+ * lặp và tăng leng +1 mỗi vòng
+*/
+function leng (object : Object): Number {
+    let leng : number = 0;
+    for(let key in object){
+        leng += 1;
+    }
+  return leng;
+}
+console.log(leng(people));
+
+/** dùng lodash */
+let arrayKey =_.keys(people);
+console.log(arrayKey);
+console.log(arrayKey.length);
+
+/**func in ra các prop của 1 object
+ * @param object: object đầu vào
+ * chạy từ đầu đến cuối, mỗi vòng thêm prop và '-' vào chuỗi string, không lấy dấu '-' cuối cùng
+ */
+function showProp(object: Object){
+    let s : string = '', checkLength: number= 0;
+    for(let key in object){
+        checkLength += 1;
+        s = s + key
+        if (checkLength < leng(object)) {
+            s = s + '-'
+            }
+    }
+return s
+}
+console.log(showProp(people));
+/** dùng lodash */
+console.log(_.join(arrayKey,'-'));
+
+
+/** func in ra tên sách, tác giả, status với tham số vào là tên cuốn sách
+ * @param name: tên cuốn sách, @param listBook: mảng cần tra cứu
+ * dựa vào name tìm trong mảng object có object.name đó và lấy ra thông tin
+ */
+function showInfo(listBook: Array<any>, name: string): string{
+    if (listBook.length === 0) {
+      return 'Không tìm thấy cuốn sách'
+    }
+    for (let i = 0; i < listBook.length; i++) {
+        if (listBook[i].name===name) {
+            return `Sách có tên là ${name}, được viết bởi ${listBook[i].author}, cuốn sách này ${listBook[i].is_read? 'đã đọc' : 'chưa đọc'}.`
+        } 
+    }
+        return 'Không tìm thấy cuốn sách';
+  }
+  console.log(showInfo(library,'Book 01'));
+
+  /** dùng lodash */
+  function showInfoUseLodash(listBook: Array<any>, name: string): string{
+    let index = _.findKey(listBook,{name: `${name}`})
+    if (index){
+        return `Sách có tên là ${name}, được viết bởi ${listBook[index].author}, cuốn sách này ${listBook[index].is_read? 'đã đọc' : 'chưa đọc'}.`
+    } 
+    return 'Không tìm thấy cuốn sách'; 
+  }
+  console.log(showInfoUseLodash(library,'Book 01'));
+
+/** func tạo 1 array mới với key admin
+ * @param listBook mảng ban đầu
+ * thêm từng object và key mới và value cũ vào mảng mới
+ */
+function changeInToAdmin(listBook: Array<any>): Array<any> {
+    if (listBook.length === 0) {
+        return []
+    }
+    let newLibrary : Array<any> =[]
+    for (let i = 0; i < listBook.length; i++) {
+         let object = {
+        admin_name :listBook[i].name,
+        admin_author:listBook[i].author,
+        admin_is_read: listBook[i].is_read,
+    };
+        newLibrary[newLibrary.length] = object;
+    }
+    return newLibrary
+}
+
+console.log(changeInToAdmin(library));
+
+/** dùng lodash */
+function changeInToAdminUseLodash(listBook: Array<any>): Array<any> {
+    let newLibrary : Array<any> =[]
+    for (let i = 0; i < listBook.length; i++) {
+        let object = _.mapKeys(listBook[i],function(value:any,key:any) {
+            return 'admin_'+ key
+        })
+       newLibrary[newLibrary.length] = object;
+   }
+
+    return newLibrary
+}
+
+console.log(changeInToAdminUseLodash(library));
+
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CodeBase
 Code Base
 
-Xử lí Array với Lodash _
-1. _.findIndex 
+# Xử lí Array với Lodash _
+1. _.findIndex object
         * _.findIndex(array, [predicate], [fromIndex=0])
         * trả về chỉ mục của phần tử đầu tiên có predicate trả về true 
         * array là mảng, [predicate] là func được gọi mỗi lần lặp, [fromIndex=0] là giá trị muốn bắt đầu tìm
@@ -35,4 +35,40 @@ Xử lí Array với Lodash _
         * thêm vào cuối mảng bất kì value nào, không phân biệt type
 10. _.take
         * _.take(array, [n=1])
-        * tạo mảng với n phần tử từ vị trí đầu, mặc định n=1
+        * tạo mảng mới từ  mảng cũ với n phần tử từ vị trí đầu, mặc định n=1
+11. _.join(array, [separator=','])
+        * chuyển đổi tất cả các phần tử trong mảng thành 1 chuỗi được phân cách với nhau bởi [separator]
+12. _.keys(object)
+        * tạo ra mảng mới chứa các phần tử của object
+13. _.keysIn(object)
+        * tạo ra mảng mới chứa các phần tử và được kế thừa(prototype) của object
+
+
+# Object
+ var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 55,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+1. Definition 
+        object ={[key: value]}
+2. Accessing Object Properties
+        2 cách: objectName.propertyName
+                objectName["propertyName"]
+3. Object Method 
+         A method is a function stored as a property in object.
+4. Accessing Object Methods
+        objectName.methodName()
+ví dụ : name = person.fullName();
+5. This keyword
+        * trong 1 định nghĩa hàm, this đề cập đến chủ sở hữu của function
+        * It has different values depending on where it is used:
+                - In a method, "this" refers to the owner object. : Trong một phương thức, this tham chiếu đến đối tượng chủ sở hữu .
+                - Alone, "this" refers to the global object. : Một mình, this đề cập đến đối tượng toàn cục .
+                - In a function, "this" refers to the global object. : Trong một hàm, this tham chiếu đến đối tượng toàn cục .
+                - In a function, in strict mode, "this" is undefined. : Trong một chức năng, trong chế độ nghiêm ngặt, this là undefined.
+                - In an event, "this" refers to the element that received the event. : Trong một sự kiện, this đề cập đến phần tử đã nhận sự kiện.
+                - Methods like call(), and apply() can refer "this" to any object. : Các phương thức như call(), và apply()có thể tham chiếu this đến bất kỳ đối tượng nào .
